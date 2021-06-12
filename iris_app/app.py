1072f7bbea161e0a7eeee9be8157a20336d09645
Jorrit de Vries <jorritdevries@pm.me>
2021-06-12 22:04:33 +0200

    refactor for code splitting

--- iris_app/app.py
+++ iris_app/app.py
@@ -1,63 +1,29 @@
-from flask import Flask, render_template, request
-import numpy as np
-import joblib
+from typing import Optional, Dict, List
+from flask import Flask, Request, Response, render_template
 
-#create instance of Flask
+from services.errors import get_request_errors
+from services.prediction_service import get_prediction
+
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return render_template('home.html')
+
+@app.route("/")
+def render_home_page(request: Request):
+    return render_template("home.html")
 
 
-@app.route('/predict/', methods=['GET','POST'])
-def predict():
+@app.route("/predict/", methods=["GET","POST"])
+def predict(request: Request):
     
-    if request.method == "POST":
-        
-        #get form data
-        sepal_length = request.form.get('sepal_length')
-        sepal_width = request.form.get('sepal_width')
-        petal_length = request.form.get('petal_length')
-        petal_width = request.form.get('petal_width')
-        
-        
-        #call preprocessDataAndPredict and pass inputs
-        try:
-            prediction = preprocessDataAndPredict(sepal_length,       sepal_width, petal_length, petal_width)            #pass prediction to template
-            return render_template('predict.html', prediction = prediction)
-   
-        except ValueError:
-            return "Please Enter valid values"
-  
-        pass
-    pass
+    request_error: Optional[str] = get_request_errors(request)
+    
+    if not request_error:
+        petal_measures: Dict[str, float] = request.get("form")
+        prediction: str = get_prediction(petal_measures)
+        return render_template("predict.html", prediction = prediction)
 
-def preprocessDataAndPredict(sepal_length, sepal_width, petal_length, petal_width):
-    
-    #keep all inputs in array
-    test_data = [sepal_length, sepal_width, petal_length, petal_width]
-    print(test_data)
-    
-    #convert value data into numpy array
-    test_data = np.array(test_data)
-    
-    #reshape array
-    test_data = test_data.reshape(1,-1)
-    print(test_data)
-    
-    #open file
-    file = open("output/randomforest_model.pkl","rb")
-    
-    #load trained model
-    trained_model = joblib.load(file)
-    
-    #predict
-    prediction = trained_model.predict(test_data)
-    
-    return prediction
-    
-    pass
+    return request_error
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
